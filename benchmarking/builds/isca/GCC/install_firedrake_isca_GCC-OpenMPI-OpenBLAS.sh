8b8ed84c175c0433e73b92905753173c434a6ddd
dma204 <d.m.acreman@exeter.ac.uk>
2021-07-27 12:07:37 +0100

    Add Isca build with GCC and OpenBLAS

---
+++ benchmarking/builds/isca/GCC/install_firedrake_isca_GCC-OpenMPI-OpenBLAS.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script for installing Firedrake on Isca (University of Exeter HPC system)
+# This version uses the GCC toolchain
+# D. Acreman, October 2019
+
+echo "Setting up modules"
+module purge
+module load Python/3.6.6-foss-2018b
+module load CMake/3.13.3-GCCcore-7.3.0
+module load Bison/3.0.5-GCCcore-7.3.0
+module load flex/2.6.4-GCCcore-7.3.0
+module load libxml2/2.9.8-GCCcore-7.3.0
+module list
+
+# Make sure PYTHONPATH is not set otherwise install script will bail out
+unset PYTHONPATH
+
+# Tell PETSc to use OpenBLAS from Easy Build installation
+export PETSC_CONFIGURE_OPTIONS=" --with-blaslapack-dir=${EBROOTOPENBLAS}"
+
+echo "Fetching install script"
+curl -O https://raw.githubusercontent.com/firedrakeproject/firedrake/master/scripts/firedrake-install
+echo "Installing"
+python3 firedrake-install --disable-ssh --no-package-manager --verbose --mpicc=mpicc --mpicxx=mpicxx --mpif90=mpifort --mpiexec=mpirun $@
+
+echo "Done"
